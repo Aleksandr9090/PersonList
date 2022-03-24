f4f67614d35777ba0dbed92a17c66b5a50f570ab
shutkin.90 <shutkin.90@mail.ru>
2022-03-24 12:50:55 +0300

    Initial Commit

---
+++ PersonList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonList
+//
+//  Created by Aleksandr on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
